b7437c0d36c36b77a484138341bd9b2e4b7076da
JoyLynn <joylynnious@gmail.com>
2015-08-19 11:03:04 +0300

    added another file that is an ssv..

--- file.rb
+++ file.rb
@@ -9,3 +9,7 @@ end
 a_string = "Dan,34\nMaria,55"
 CSV.parse(a_string) { |row| puts row.inspect } 
 #=> produces ["Dan", "34"] and ["Maria", "55"] on separate lines
+
+CSV.parse(File.read('customers.csv')) 
+# File.read returns a (big) string of the data in 'customers.csv', comma-separated
+# and CSV.parse just re-structures that data into an array data structure.

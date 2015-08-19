9d78a3fcd001ee1c47a267a3543a64055c8a9eea
JoyLynn <joylynnious@gmail.com>
2015-08-19 11:21:34 +0300

    added a method that will calculate the average money spent for each arrival..

--- file.rb
+++ file.rb
@@ -1,10 +1,13 @@
 require 'csv'
 
-customers = CSV.read('customers.csv', { :col_sep => ';' })
+customers = CSV.read('customers.csv')
 
-CSV.foreach('customers.csv') do |row|
-  puts row.inspect
-end
+average_money_spent = Array.new
+CSV.foreach('customers.csv', converters: :numeric) do |row|
+  # puts row.inspect
+   average_money_spent << row[2] / row[1]
+    # row is just an ordinary array and you access its elements with []
+  end #=> Undefined method '/' for "2548":String
 
 a_string = "Dan,34\nMaria,55"
 CSV.parse(a_string) { |row| puts row.inspect } 
@@ -13,3 +16,15 @@ CSV.parse(a_string) { |row| puts row.inspect }
 CSV.parse(File.read('customers.csv')) 
 # File.read returns a (big) string of the data in 'customers.csv', comma-separated
 # and CSV.parse just re-structures that data into an array data structure.
+
+
+new_customers = CSV.read('newcomers.csv', { :col_sep => ';' })
+CSV.foreach('newcomers.csv', { :col_sep => ';' }) { |row| p row }
+
+CSV.foreach('newcomers.csv', col_sep: ';') { |row| p row }
+a_string = "Dan;34\nMaria;55"
+CSV.parse(a_string, col_sep: ';') #=> [["Dan", "34"], ["Maria", "55"]]
+
+
+  CSV.foreach('customers.csv') do |row|
+   

585cf6b0cd76355cf9141e7b68bdcd2fda4dfaf2
JoyLynn <joylynnious@gmail.com>
2015-08-19 10:55:49 +0300

    added a csv parse method with a block..

--- file.rb
+++ file.rb
@@ -5,3 +5,7 @@ customers = CSV.read('customers.csv')
 CSV.foreach('customers.csv') do |row|
   puts row.inspect
 end
+
+a_string = "Dan,34\nMaria,55"
+CSV.parse(a_string) { |row| puts row.inspect } 
+#=> produces ["Dan", "34"] and ["Maria", "55"] on separate lines
